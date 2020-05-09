70de2e3ea1dd5f54ce030a3602029e826fa81907
zhenyuan <936451076@qq.com>
2020-05-09 16:06:55 +0800

    fix：关闭debug模式

--- userSystem/settings.py
+++ userSystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4%weu=%l0_moeocq$*3a&*mx)a)nutod(#g5=r-8ey%xyu@cam'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
