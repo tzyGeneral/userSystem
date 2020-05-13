3b5c712832a50507ab7817a2f14777cc9d270afb
zhenyuan <936451076@qq.com>
2020-05-13 23:03:12 +0800

    add：权限控制添加请求方式的判断

--- userSystem/tool/permissionCheck.py
+++ userSystem/tool/permissionCheck.py
@@ -5,10 +5,31 @@ from userSystem.cache.datacache import RolesCache
 class AddUserPermission(object):
 
     def has_permission(self, request, view):
+        requestMethod = request.method
+
         cacheTool = RolesCache(model=UserInfo)
         key = 'rolesCache+' + str(request.user)
         roleList = cacheTool.getRolesFromCache(key)
 
-        if '管理员' in roleList:
+        if requestMethod == 'GET':
+
+            # if '管理员' in roleList:
             return True
+
+        elif requestMethod == 'POST':
+
+            if '管理员' in roleList:
+                return True
+
+        elif requestMethod == 'PUT':
+
+            if '管理员' in roleList:
+                return True
+        elif requestMethod == 'DELETE':
+
+            if '管理员' in roleList:
+                return True
+        else:
+            return False
+
         return False
