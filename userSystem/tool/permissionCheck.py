562f2820e84a57b74d2e2f86e6a59125d5839ba2
zhenyuan <936451076@qq.com>
2020-05-14 10:17:54 +0800

    fix：权限验证从token直接获取，不走缓存

--- userSystem/tool/permissionCheck.py
+++ userSystem/tool/permissionCheck.py
@@ -7,9 +7,11 @@ class AddUserPermission(object):
     def has_permission(self, request, view):
         requestMethod = request.method
 
-        cacheTool = RolesCache(model=UserInfo)
-        key = 'rolesCache+' + str(request.user)
-        roleList = cacheTool.getRolesFromCache(key)
+        # 这里可能有的问题，就是用户被修改了角色之后，无法立即刷新，可能还是需要每次验证，查询一次数据库
+        userObj = request.auth
+        roles = userObj.roles.all()
+        # 这里可以改成 x.id ，然后用id去判断是否有权限
+        roleList = [x.name for x in roles]
 
         if requestMethod == 'GET':
 
