f1d29bd61562671b1714cca1f5f49ae855484a04
zhenyuan <936451076@qq.com>
2020-05-21 12:11:01 +0800

    fix：缓存键添加前缀

--- userSystem/cache/cacheTool.py
+++ userSystem/cache/cacheTool.py
@@ -10,9 +10,10 @@ def checkUserNmaeTool(userName: str):
     :param userName:
     :return:
     """
-    if cache.has_key(userName):
+    key = 'usernameCache+' + str(userName)
+    if cache.has_key(key):
         # 缓存中有则返回user_id
-        return cache.get(userName)
+        return cache.get(key)
     else:
         # 没有返回None，视图层再做操作
         return None
@@ -24,7 +25,8 @@ def checkUserInfoTool(user_id: str):
     :param user_id:
     :return:
     """
-    if cache.has_key(user_id):
+    key = 'userIdCache+' + str(user_id)
+    if cache.has_key(key):
         # 缓存中存在则返回True
         return True
     else:
@@ -38,11 +40,12 @@ def setUsername2UseridToCache(username: str):
     :param username:
     :return:
     """
+    key = 'usernameCache+' + str(username)
     # 通过用户名user_name查询用户的user_id
     user = UserInfo.objects.get(username=username)
     if user:
         # 将user_id存入缓存中， key：user_name, value: user_id
-        DataCache(timeout=60 * 60 * 3).setCache(key=username, value=user.id)
+        DataCache(timeout=60 * 60 * 3).setCache(key=key, value=user.id)
         return user.id
 
 def setUserid2UserInfoToCache(user_id: str):
@@ -51,6 +54,7 @@ def setUserid2UserInfoToCache(user_id: str):
     :param user_id:
     :return:
     """
+    key = 'permissionCache+' + str(user_id)
     # 通过用户的id获取该用户的角色信息
     user = UserInfo.objects.get(id=user_id)
     # 该用户关联的角色（用户角色多对多）
@@ -68,4 +72,4 @@ def setUserid2UserInfoToCache(user_id: str):
         result += permission.data
 
     # 将user_info(包含角色，权限)存入缓存中
-    DataCache(timeout=60 * 60 * 3).setCache(key=user_id, value=result)
+    DataCache(timeout=60 * 60 * 3).setCache(key=key, value=result)
