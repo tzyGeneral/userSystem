8e266d40d40e77e001c3d0c4feac98a57c17cde8
zhenyuan <936451076@qq.com>
2020-05-21 16:25:23 +0800

    add：cachetool优化

--- userSystem/cache/cacheTool.py
+++ userSystem/cache/cacheTool.py
@@ -1,6 +1,5 @@
-from django.core.cache import cache
 from userSystem.models import *
-from userSystem.cache.datacache import DataCache
+from userSystem.cache.datacache import DataCache2
 from userSystem.serialzer import PermissionsSerializer
 
 
@@ -10,10 +9,10 @@ def checkUserNmaeTool(userName: str):
     :param userName:
     :return:
     """
-    key = 'usernameCache+' + str(userName)
-    if cache.has_key(key):
+    cacheTool = DataCache2(cacheName='user_id_cache', timeout=60*60*3)
+    if cacheTool.checkKey(userName):
         # 缓存中有则返回user_id
-        return cache.get(key)
+        return cacheTool.getCache(key=userName)
     else:
         # 没有返回None，视图层再做操作
         return None
@@ -21,12 +20,12 @@ def checkUserNmaeTool(userName: str):
 
 def checkUserInfoTool(user_id: str):
     """
-    查询 user_info -> user_id 的缓存
+    查询 user_id -> user_info 的缓存
     :param user_id:
     :return:
     """
-    key = 'userIdCache+' + str(user_id)
-    if cache.has_key(key):
+    cacheTool = DataCache2(cacheName='user_info_cache', timeout=60*60*3)
+    if cacheTool.checkKey(user_id):
         # 缓存中存在则返回True
         return True
     else:
@@ -40,13 +39,14 @@ def setUsername2UseridToCache(username: str):
     :param username:
     :return:
     """
-    key = 'usernameCache+' + str(username)
+    cacheTool = DataCache2(cacheName='user_id_cache', timeout=60*60*3)
     # 通过用户名user_name查询用户的user_id
     user = UserInfo.objects.filter(username=username).first()
     if user:
         # 将user_id存入缓存中， key：user_name, value: user_id
-        DataCache(timeout=60 * 60 * 3).setCache(key=key, value=user.id)
+        cacheTool.setCache(key=username, value=user.id)
         return user.id
+
 
 def setUserid2UserInfoToCache(user_id: str):
     """
@@ -54,7 +54,7 @@ def setUserid2UserInfoToCache(user_id: str):
     :param user_id:
     :return:
     """
-    key = 'permissionCache+' + str(user_id)
+    cacheTool = DataCache2(cacheName='user_permission_cache', timeout=60*60*3)
     # 通过用户的id获取该用户的角色信息
     user = UserInfo.objects.get(id=user_id)
     # 该用户关联的角色（用户角色多对多）
@@ -72,4 +72,33 @@ def setUserid2UserInfoToCache(user_id: str):
         result += permission.data
 
     # 将user_info(包含角色，权限)存入缓存中
-    DataCache(timeout=60 * 60 * 3).setCache(key=key, value=result)
+    cacheTool.setCache(key=user_id, value=result)
+
+
+def getPermissionFromCache(user_id: str):
+    """
+    通过 user_id 获取 user_info 的权限
+    :param self:
+    :param key:
+    :return:
+    """
+    cacheTool = DataCache2(cacheName='user_permission_cache', timeout=60*60*3)
+    result = cacheTool.getCache(user_id)
+    if not result:
+        # 通过用户的id获取该用户的角色信息
+        user = UserInfo.objects.get(id=user_id)
+        # 该用户关联的角色（用户角色多对多）
+        role = user.roles.all()
+        # 该用户单独关联的权限（用户角色多对多）
+        userPermission = user.permissions.all()
+        userPermission = PermissionsSerializer(userPermission, many=True)
+        result = userPermission.data
+        rolesIdList = [x.id for x in role]
+        # 通过角色信息获取所有的权限（角色权限多对多）
+        for onerole in rolesIdList:
+            permission = Role.objects.get(id=onerole)
+            permission = permission.permissions.all()
+            permission = PermissionsSerializer(permission, many=True)
+            result += permission.data
+        cacheTool.setCache(key=user_id, value=result)
+    return result
